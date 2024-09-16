1b06c0981befcedaf056efb4ac42e42d8d3bc1b8
Muth1212 <156949366+Muth1212@users.noreply.github.com>
2024-09-16 10:13:55 -0700

    Update app.js

--- Web Blog App/public/app.js
+++ Web Blog App/public/app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let posts = [];
+
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
 
